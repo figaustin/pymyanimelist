6cbcd6533e29251595ce693b329d4b3831cd85f9
Osvaldo Figueroa <figaustin@yahoo.com>
2023-07-28 00:40:49 -0400

    new test

--- tests/test_animesearch.py
+++ tests/test_animesearch.py
@@ -11,8 +11,8 @@ class TestAnimeSearch(unittest.TestCase):
         load_dotenv()
         mal = pymyanimelist.MAL(client_id=os.getenv('CLIENT_ID'), client_secret=os.getenv('CLIENT_SECRET'))
 
-        s = mal.search(search_type="anim", search="akame")
-        print(s)
+        s = mal.search(search_type="anime", search="akame")
+        print(s.data[0].node.title)
 
 if __name__ == '__main__':
     unittest.main()
